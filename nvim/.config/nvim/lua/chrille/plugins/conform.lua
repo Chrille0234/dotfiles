10138ceff888e327211173493299abe38abf8529
chrille0234 <madsen.chr@hotmail.com>
2025-01-18 21:24:48 +0100

    improve NVIM code formatting

---
+++ nvim/.config/nvim/lua/chrille/plugins/conform.lua
@@ -0,0 +1,28 @@
+return {
+  "stevearc/conform.nvim",
+  event = { "BufReadPre", "BufNewFile" },
+  config = function()
+    local conform = require("conform")
+
+    conform.setup({
+      formatters_by_ft = {
+        javascript = { "prettier" },
+        typescript = { "prettier" },
+        javascriptreact = { "prettier" },
+        typescriptreact = { "prettier" },
+        svelte = { "prettier" },
+        css = { "prettier" },
+        html = { "prettier" },
+        json = { "prettier" },
+        csharp = { "csharpier" },
+      },
+      vim.keymap.set({ "v"}, "=", function()
+        conform.format({
+          lsp_fallback = true,
+          async = false,
+          timeout_ms = 500,
+        })
+      end, { desc = "Format file or range (in visual mode)" })
+    })
+  end,
+}
