3b08c64f208cd0af25b53eaea2ab99e36b4a2dda
chrille0234 <madsen.chr@hotmail.com>
2025-03-01 00:41:34 +0100

    remove useless comments

--- nvim/.config/nvim/lua/chrille/plugins/harpoon.lua
+++ nvim/.config/nvim/lua/chrille/plugins/harpoon.lua
@@ -1,5 +1,5 @@
  return {
-   -- Specify the plugin repository
+   -- Specify the plugin repositoryharp
    "ThePrimeagen/harpoon",
    -- Plugin configuration
    config = function()
@@ -18,11 +18,9 @@
          },
        },
      })
- 
      -- Optionally set up keybindings
      local map = vim.api.nvim_set_keymap
      local opts = { noremap = true, silent = true }
- 
      -- Map keys to Harpoon commands
      map("n", "<leader>a", ":lua require('harpoon.mark').add_file()<CR>", opts)
      map("n", "<leader>m", ":lua require('harpoon.ui').toggle_quick_menu()<CR>", opts)
@@ -30,13 +28,9 @@
      map("n", "<leader>2", ":lua require('harpoon.ui').nav_file(2)<CR>", opts)
      map("n", "<leader>3", ":lua require('harpoon.ui').nav_file(3)<CR>", opts)
      map("n", "<leader>4", ":lua require('harpoon.ui').nav_file(4)<CR>", opts)
- 
      map("n", "<leader>r1", ":lua require('harpoon.mark').set_current_at(1)<CR>", opts)
      map("n", "<leader>r2", ":lua require('harpoon.mark').set_current_at(2)<CR>", opts)
      map("n", "<leader>r3", ":lua require('harpoon.mark').set_current_at(3)<CR>", opts)
      map("n", "<leader>r4", ":lua require('harpoon.mark').set_current_at(4)<CR>", opts)
- 
-     -- Add more keybindings as necessary
    end
  }
- 
