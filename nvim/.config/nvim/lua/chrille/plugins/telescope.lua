4108aee88c497442681950587e22058accbb35a3
chrille0234 <madsen.chr@hotmail.com>
2025-03-01 00:26:57 +0100

    make telescope prettier

--- nvim/.config/nvim/lua/chrille/plugins/telescope.lua
+++ nvim/.config/nvim/lua/chrille/plugins/telescope.lua
@@ -1,16 +1,43 @@
 return {
-	"nvim-telescope/telescope.nvim",
+  {
+    "nvim-telescope/telescope.nvim",
 
-	tag = "0.1.5",
+    tag = "0.1.5",
 
-	dependencies = {"nvim-lua/plenary.nvim"},
+    dependencies = { "nvim-lua/plenary.nvim" },
 
-	config = function()
-		require("telescope").setup({})
+    config = function()
+      require("telescope").setup({
+        defaults = {
+          layout_config = {
+            vertical = {
+              width = 0.8,
+              height = 0.8,
+              preview_height = 0.6,
+              preview_width = 0.9,
+            },
+          },
+          border = true,
+          borderchars = { '─', '│', '─', '│', '┌', '┐', '┘', '└' },
+          preview = {
+            lines = 10,
+            winblend = 0,
+            number = true,
+          }
+        },
+      })
 
-		local builtin = require("telescope.builtin")
+      local builtin = require("telescope.builtin")
 
-	        vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
-        	vim.keymap.set('n', '<C-p>', builtin.git_files, {})
-	end
+      vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
+      vim.keymap.set('n', '<C-p>', builtin.git_files, {})
+      vim.keymap.set('n', '<leader>ps', function()
+        builtin.grep_string({ search = vim.fn.input("Grep > ") })
+      end)
+    end
+
+  },
+  {
+    "tpope/vim-rhubarb"
+  }
 }
