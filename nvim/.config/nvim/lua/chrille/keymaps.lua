35e9d0e04c4ef6f6fa9129655c2725f62271306b
chrille0234 <madsen.chr@hotmail.com>
2025-03-01 00:40:03 +0100

    add rename shortcut and make cursor center when pressing U and D

--- nvim/.config/nvim/lua/chrille/keymaps.lua
+++ nvim/.config/nvim/lua/chrille/keymaps.lua
@@ -3,6 +3,9 @@ vim.g.mapleader = " "
 
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 vim.keymap.set("n", "<leader>ex", vim.cmd.exit)
+vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename)
+vim.keymap.set("n", "<C-u>", "<C-u>zz")
+vim.keymap.set("n", "<C-d>", "<C-d>zz")
 vim.keymap.set("n", "gd", function ()
   vim.lsp.buf.definition()
   vim.defer_fn(function()
